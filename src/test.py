cd8f81e91bef76161c19b6e5a723a23c068f95c6
oli-dev-ai <oopyrchal@pm.me>
2025-12-11 18:58:41 +0100

    Clean code things...

--- src/test.py
+++ src/test.py
@@ -1,8 +1,5 @@
 from opencv import load_image, convert_to_grayscale, display_image, denoise
 
-import cv2
-import matplotlib.pyplot as plt
-
 if __name__ == "__main__":
     img = load_image("../data/samples/samplejop.jpg")
     gray = convert_to_grayscale(img)
