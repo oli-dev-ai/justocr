dd2e870f02490365c11f4f80f254fd3437aa0d54
oli-dev-ai <oopyrchal@pm.me>
2025-12-17 18:49:34 +0100

    Autosaving preprocessed img and ocr txt, with suffix

--- src/ocrtest.py
+++ src/ocrtest.py
@@ -1,14 +1,32 @@
 import pytesseract
 import pipeline
+from pathlib import Path
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+def save_ocr(text, original_image_path):
+    """
+    Zapisuje wynik OCR do txt.
+    
+    Args:
+        text (str): Wyekstraktowany tekst
+        original_image_path (str): Ścieżka do oryginalnego obrazu
+    """
+    output_dir = Path("../data/txt_reco")             #gdzie ma się zapisać
+    original_image_path = Path(original_image_path)   #oryginala scieżka pliku do ocr
+    base_name = original_image_path.stem              #nazwa bazowa -> original img
+    txt_filename = f"{base_name}_txt_proc.txt"        #nazwa -> nazwa bazowa + txt_proc
+    txt_path = output_dir / txt_filename              #gdzie ma się zapisać i jako co
 
+    with open(txt_path,'w', encoding='utf-8') as f:
+        f.write(text)
 
 def testocr(image_path):
+    """OCR z automatycznym zapisem img preprocessed oraz txt"""
     image = pipeline.process_pipeline(image_path)
-    text = pytesseract.image_to_string(image, lang="eng+pol")
-    print(text)
+    text = pytesseract.image_to_string(image, lang="pol")
+    save_ocr(text, image_path)  #uzycie funkcji zapisania do txt
+
     return text
 
 if __name__=="__main__":
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,4 +1,6 @@
 from opencv import load_image, convert_to_grayscale, display_image, denoise, threshold, correct_rotation
+import cv2
+from pathlib import Path
 
 def process_pipeline(image_path):
     img = load_image(image_path)
@@ -6,4 +8,24 @@ def process_pipeline(image_path):
     denoised = denoise(gray)
     thresholded = threshold(denoised)
     corrected_rot = correct_rotation(thresholded)
+
+    output_dir = Path("../data/processed")
+
+    input_path = Path(image_path)
+    base_name = input_path.stem     #wyciągniecie nazwy pliku bez rozszerzenia
+    extension = input_path.suffix   #wyciągnięcie tylko rozszerzenia pliku
+
+    counter = 1
+    while True:
+        output_filename = f"{base_name}_processed_{counter}{extension}"
+        output_path = output_dir / output_filename   # Operator / łączy ścieżki 
+        
+        if not output_path.exists():                 #sprawdza czy plik nie istnieje, jesli true to zostawia obecny numer
+            break
+        
+        counter += 1
+    
+    cv2.imwrite(str(output_path), corrected_rot)
+    print(f"Saved in{output_path}")
+
     return corrected_rot
